9cd8631b80efe2f1f61075a43278c3b7706a176c
Sergey Chupov <sergey.chupov@themidnightcoders.com>
2015-03-28 09:58:58 +0200

    [KiTo-2]: rewrote parser in java-7 style

--- parsers/src/main/java/kino/today/parsers/KievCinemaParser.java
+++ parsers/src/main/java/kino/today/parsers/KievCinemaParser.java
@@ -13,50 +13,57 @@ import java.util.stream.Collectors;
  * Created by hlib on 24.03.15.
  */
 //TODO extract interface from here
-public class KievCinemaParser {
+public class KievCinemaParser
+{
 
+  //TODO think where to put xPathes below
+  private final static String xPathToSessionLink = ".session.actual";
 
-    //TODO think where to put xPathes below
-    private final static String xPathToSessionLink = ".session.actual";
+  private final static String xPathToTitle = "div.right-column.center>h3";
+  private final static String xPathToTime = "div.right-column.center>p";
+  private final static String xPathToPrices = "ul#scheme-legend>li";
 
-    private final static String xPathToTitle = "div.right-column.center>h3";
-    private final static String xPathToTime = "div.right-column.center>p";
-    private final static String xPathToPrices = "ul#scheme-legend>li";
+  Map<String, List<String>> movieNameToSessions = new HashMap<>();
 
-    Map<String, List<String>> movieNameToSessions = new HashMap<>();
+  public void parse() throws IOException
+  {
+    //TODO put the url in properties or something
+    //String url = new Crawler().getSessionsPageLink();
+    String url = "http://www.kievkino.com.ua/ru/sessions/2/index.html";
 
-    public void parse() throws IOException {
-        //TODO put the url in properties or something
-        //String url = new Crawler().getSessionsPageLink();
-        String url = "http://www.kievkino.com.ua/ru/sessions/2/index.html";
+    Document document = Jsoup.connect( url ).get();
+    Elements elements = document.select( xPathToSessionLink );
+    for( Element element : elements )
+    {
+      String linkToSession = element.attr( "abs:href" );
+      parseSession( linkToSession );
+    }
+  }
 
-        Document document = Jsoup.connect(url).get();
-        Elements elements = document.select(xPathToSessionLink);
-        for (Element element : elements) {
-            String linkToSession = element.attr("abs:href");
-            parseSession(linkToSession);
-        }
+  private void parseSession( String linkToSession ) throws IOException
+  {
+    Document document = Jsoup.connect( linkToSession ).get();
+
+    String title = document.select( xPathToTitle ).first().text();
+    String time = document.select( xPathToTime ).first().text();
+    List<Element> elements = document.select( xPathToPrices );
+    List<String> prices = new ArrayList<>(  );
+    for( Element element : elements )
+    {
+      prices.add( element.text() );
     }
 
-    private void parseSession(String linkToSession) throws IOException {
-        Document document = Jsoup.connect(linkToSession).get();
+    put( title, time + "--- " + prices + " ---" );
+  }
 
-        String title = document.select(xPathToTitle).first().text();
-        String time = document.select(xPathToTime).first().text();
-        List<String> prices = document.select(xPathToPrices).stream().map(
-                element -> element.text()
-        ).collect(Collectors.toList());
-
-        put(title, time + "--- " + prices + " ---");
-
+  private void put( String name, String time )
+  {
+    List<String> list = movieNameToSessions.get( name );
+    if( list == null )
+    {
+      list = new ArrayList<>();
+      movieNameToSessions.put( name, list );
     }
-
-    private void put(String name, String time) {
-        List<String> list = movieNameToSessions.get(name);
-        if (list == null) {
-            list = new ArrayList<>();
-            movieNameToSessions.put(name, list);
-        }
-        list.add(time);
-    }
+    list.add( time );
+  }
 }
